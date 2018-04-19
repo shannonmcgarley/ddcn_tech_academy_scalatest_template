e29b82fcd10d62f60605179d2752e6ef085db66c
shannon mcgarley <shannon.mcgarley@digital.hmrc.gov.uk>
2018-04-19 17:16:54 +0100

    tests pass and can split and rejoin strings given an index

--- src/main/scala/Hello.scala
+++ src/main/scala/Hello.scala
@@ -1,5 +1,14 @@
-object Hello extends App {
+object chromosomeCrossover extends App {
 
-  def greet : String = "hello"
+  def crossing(Chrome1 : String, Chrome2 : String, Index : Int) : (String, String) ={
+   val Chrome1Head = Chrome1.substring(0,Index)
+    val Chrome1Tail = Chrome1.substring(Index,Chrome1.length)
+    val Chrome2Head = Chrome2.substring(0,Index)
+    val Chrome2Tail = Chrome2.substring(Index,Chrome2.length)
+    val Chrome1Cross = Chrome1Head+Chrome2Tail
+    val Chrome2Cross = Chrome2Head+Chrome1Tail
+    (Chrome1Cross, Chrome2Cross)
+  }
 
 }
+//Chrome1.substring()
--- src/test/scala/HelloSpec.scala
+++ src/test/scala/HelloSpec.scala
@@ -4,8 +4,14 @@ class HelloSpec extends WordSpec with MustMatchers {
 
   "Hello" must {
 
-    "say hello" in {
-      Hello.greet mustEqual "hello"
+    "split and cross a pair of chromosomes that are 1 in length" in {
+      chromosomeCrossover.crossing("0","1", 0) mustEqual ("1", "0")
+    }
+    "split and cross a pair of chromosomes that are 4 in lenth" in{
+      chromosomeCrossover.crossing("1111", "0000", 1) mustEqual ("1000", "0111")
+    }
+    "split and cross a pair of chromosomes that are 7 in length" in{
+      chromosomeCrossover.crossing("1111111","0000000", 3) mustEqual ("1110000", "0001111")
     }
 
   }
